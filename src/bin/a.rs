8d6b16eae344e2c3cbc327007001c678412b8a66
masa <masatoshi.sboard@gmail.com>
2021-03-18 14:49:31 +0900

    Done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,34 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        h: usize,
+        w: usize,
+        s: [Chars; h],
     }
+    let dx = vec![1, 0];
+    let dy = vec![0, 1];
+    let mut dp = vec![vec![std::usize::MAX; w]; h];
+    if s[0][0] == '#' {
+        dp[0][0] = 1;
+    } else {
+        dp[0][0] = 0;
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            for k in 0..2 {
+                let ci = i + dx[k];
+                let cj = j + dy[k];
+                if ci >= h || cj >= w {
+                    continue;
+                }
+                let mut add = 0;
+                if s[ci][cj] == '#' && s[i][j] == '.' {
+                    add = 1;
+                }
+                dp[ci][cj] = std::cmp::min(dp[ci][cj], dp[i][j] + add);
+            }
+        }
+    }
+    println!("{}", dp[h - 1][w - 1]);
 }
